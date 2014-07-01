54a282325b351545f3cd0ecdf223828f647d1cb5
U-aureolo\dluciv <dluciv@aureolo.(none)>
2014-07-01 11:09:24 +0400

    Changed updateURL

---
+++ juick.com/everlasting-juick.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name          Everlasting Juick
+// @namespace     http://dluciv.name/
+// @description   Everlasting Juick scrolling / Бесконечный скроллинг Жуйка
+// @author        dluciv
+// @license       WTFPLv2 (http://wtfpl.net/)
+// @version       0.0.2.8
+// @icon          https://raw.githubusercontent.com/dluciv/UserScripts/master/juick.com/everlasting-juick.png
+// @homepage      https://github.com/dluciv/UserScripts/tree/master/juick.com
+// @updateURL     https://raw.githubusercontent.com/dluciv/UserScripts/master/juick.com/everlasting-juick.meta.js
+// @downloadURL   https://raw.githubusercontent.com/dluciv/UserScripts/master/juick.com/everlasting-juick.user.js
+//
+// @include       http://juick.com/*
+// ==/UserScript==
