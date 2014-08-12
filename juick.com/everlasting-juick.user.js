e8740f77811ea571e698487e2648e89595a58538
Dmitry V. Luciv <dluciv@dluciv.name>
2014-08-12 16:02:31 +0400

    Another license correction

--- juick.com/everlasting-juick.user.js
+++ juick.com/everlasting-juick.user.js
@@ -3,8 +3,10 @@
 // @namespace     http://dluciv.name/
 // @description   Everlasting Juick scrolling / Бесконечный скроллинг Жуйка
 // @author        dluciv
-// @license       Dual, select most suitable one: WTFPLv2 (http://wtfpl.net/), MIT (http://opensource.org/licenses/MIT) /Copyright (c) 2014, Dmitry V. Luciv/
-// @version       0.0.3.4
+// @copyright     2014+, Dmitry V. Luciv
+// @license       WTFPLv2; http://wtfpl.net
+// @license       MIT; http://opensource.org/licenses/MIT
+// @version       0.0.3.5
 // @icon          https://raw.githubusercontent.com/dluciv/UserScripts/master/juick.com/everlasting-juick.png
 // @homepage      https://github.com/dluciv/UserScripts/tree/master/juick.com
 // @updateURL     https://raw.githubusercontent.com/dluciv/UserScripts/master/juick.com/everlasting-juick.meta.js
