e183b708a05aab6c0928f74577b7c352343511b9
Dmitry V. Luciv <dluciv@dluciv.name>
2014-08-25 14:56:48 +0400

    Wakaba Links
    Yet another crappy script

---
+++ wakaba-links/README.md
@@ -0,0 +1,11 @@
+# Wakaba Links / Ссылки Wakaba
+
+In English:
+
+* Clicking on reply number adds its `>>...` link to reply form (as usual).
+* Context menu now allows to copy full relpy URL with `#...` reference to clipboard to use it anywhere.
+
+По-русски:
+
+* Щелчок по номеру ответа добавляет ссылку на него в виде `>>...` в форму ответа (как обычно).
+* Контекстное меню позволяет скопировать полный URL ответа, вклюая `#...`, в буфер обмена.
---
+++ wakaba-links/wakaba-links.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name          Wakaba Reply Links
+// @namespace     http://dluciv.name/
+// @description   Wakaba Reply Links. Click pastes >>... link to reply form, context menu allows to copy URL with #... reply reference
+// @namespace     http://dluciv.name/
+// @copyright     2014+, Dmitry V. Luciv
+// @license       WTFPLv2; http://wtfpl.net
+// @license       MIT; http://opensource.org/licenses/MIT
+// @version       0.0.0.2
+// @homepage      https://github.com/dluciv/UserScripts/tree/master/wakaba-links
+// @icon          https://raw.githubusercontent.com/dluciv/UserScripts/master/wakaba-links/unyl-chan.png
+// @updateURL     https://raw.githubusercontent.com/dluciv/UserScripts/master/wakaba-links/wakaba-links.user.js
+// @downloadURL   https://raw.githubusercontent.com/dluciv/UserScripts/master/wakaba-links/wakaba-links.user.js
+// @grant         none
+// @include       http://iichan.hk/*
+// ==/UserScript==
+
+try {
+
+    var replies = document.querySelectorAll('td[id^="reply"]');
+
+    for(var reply of replies)
+    {
+	var rlhref = reply.querySelector('span.reflink a[href^="javascript:insert("]');
+	console.log(rlhref.attributes['href']);
+	var rid = reply.getAttribute('id').replace('reply', '');
+	
+	rlhref.setAttribute('href', "#reply" + rid);
+	rlhref.setAttribute('onclick', 'insert(">>' + rid + '")');
+    }
+
+} catch(e) {
+    console.log(e);
+}
