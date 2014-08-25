27baa17150c3f5c32ca5632873c3dcb74b0a4872
Dmitry V. Luciv <dluciv@dluciv.name>
2014-08-25 15:01:22 +0400

    More convenient URL hash

--- wakaba-links/wakaba-links.user.js
+++ wakaba-links/wakaba-links.user.js
@@ -6,7 +6,7 @@
 // @copyright     2014+, Dmitry V. Luciv
 // @license       WTFPLv2; http://wtfpl.net
 // @license       MIT; http://opensource.org/licenses/MIT
-// @version       0.0.0.2
+// @version       0.0.0.3
 // @homepage      https://github.com/dluciv/UserScripts/tree/master/wakaba-links
 // @icon          https://raw.githubusercontent.com/dluciv/UserScripts/master/wakaba-links/unyl-chan.png
 // @updateURL     https://raw.githubusercontent.com/dluciv/UserScripts/master/wakaba-links/wakaba-links.user.js
@@ -25,7 +25,7 @@ try {
 	console.log(rlhref.attributes['href']);
 	var rid = reply.getAttribute('id').replace('reply', '');
 	
-	rlhref.setAttribute('href', "#reply" + rid);
+	rlhref.setAttribute('href', "#" + rid);
 	rlhref.setAttribute('onclick', 'insert(">>' + rid + '")');
     }
 
