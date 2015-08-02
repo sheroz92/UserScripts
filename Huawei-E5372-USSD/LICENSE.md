2fa149895d0c9a368f0ae7e289743ac614c7e7e0
Dmitry V. Luciv <dluciv@dluciv.name>
2015-08-02 21:52:44 +0300

    Megafon MR100-3 USSD tool

---
+++ Huawei-E5372-USSD/LICENSE.md
@@ -0,0 +1,6 @@
+# Лицензия / Lucense
+
+Код лицензирован двояко / Code is licensed dually:
+
+* [WTFPLv2](http://wtfpl.net);
+* [MIT](http://opensource.org/licenses/MIT) — по условиям / by TOS of [OpenUserJS](http://openuserjs.org/).
---
+++ Huawei-E5372-USSD/huawei-e5372-ussd.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name          Huawei E5372 branded USSD
+// @namespace     http://dluciv.name/
+// @description   Добавляется пункт для отправки произвольных USSD в меню GreaseMonkey для брендированных прошивок Huawei E5372 без USSD
+// @author        dluciv
+// @copyright     2015+, Dmitry V. Luciv
+// @license       WTFPLv2; http://wtfpl.net
+// @license       MIT; http://opensource.org/licenses/MIT
+// @version       0.1.0
+// @icon          https://raw.githubusercontent.com/dluciv/UserScripts/master/Huawei-E5372-USSD/ussd.png
+// @homepage      https://raw.githubusercontent.com/dluciv/UserScripts/master/Huawei-E5372-USSD
+// @updateURL     https://raw.githubusercontent.com/dluciv/UserScripts/master/Huawei-E5372-USSD/huawei-e5372-ussd.user.js
+// @downloadURL   https://raw.githubusercontent.com/dluciv/UserScripts/master/Huawei-E5372-USSD/huawei-e5372-ussd.user.js
+//
+// @grant         GM_registerMenuCommand
+//
+// @include       http://status.megafon.ru/*
+// @include       http://192.168.8.1/
+
+// ==/UserScript==
+
+(function() {
+  var doall = function() {
+    var ussd = window.prompt("Enter *USSD# code", "*100#");
+    if(ussd != null) {
+      window.sendCommonByUssd(ussd, "CodeType");
+    }
+  };
+  GM_registerMenuCommand("USSD", doall);
+}).call(this);
