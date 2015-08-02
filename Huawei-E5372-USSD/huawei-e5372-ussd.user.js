0d4f65397c379eb40abe4c23e2ca6d2f8c29c5fd
Dmitry V. Luciv <dluciv@dluciv.name>
2015-08-02 22:34:56 +0300

    Unsafe Window

--- Huawei-E5372-USSD/huawei-e5372-ussd.user.js
+++ Huawei-E5372-USSD/huawei-e5372-ussd.user.js
@@ -6,7 +6,7 @@
 // @copyright     2015+, Dmitry V. Luciv
 // @license       WTFPLv2; http://wtfpl.net
 // @license       MIT; http://opensource.org/licenses/MIT
-// @version       0.1.2
+// @version       0.1.3
 // @icon          https://raw.githubusercontent.com/dluciv/UserScripts/master/Huawei-E5372-USSD/ussd.png
 // @homepage      https://github.com/dluciv/UserScripts/tree/master/Huawei-E5372-USSD
 // @updateURL     https://raw.githubusercontent.com/dluciv/UserScripts/master/Huawei-E5372-USSD/huawei-e5372-ussd.user.js
@@ -23,7 +23,7 @@
   var doall = function() {
     var ussd = window.prompt("Enter *USSD# code", "*100#");
     if(ussd != null) {
-      window.sendCommonByUssd(ussd, "CodeType");
+      unsafeWindow.sendCommonByUssd(ussd, "CodeType");
     }
   };
   GM_registerMenuCommand("USSD", doall);
