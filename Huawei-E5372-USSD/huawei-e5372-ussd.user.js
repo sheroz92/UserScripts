3b53c07ac5c2e6f74ef62b63636bb2dd8c02d18c
Dmitry V. Luciv <dluciv@dluciv.name>
2015-08-03 13:33:20 +0300

    Common USSD replies

--- Huawei-E5372-USSD/huawei-e5372-ussd.user.js
+++ Huawei-E5372-USSD/huawei-e5372-ussd.user.js
@@ -6,7 +6,7 @@
 // @copyright     2015+, Dmitry V. Luciv
 // @license       WTFPLv2; http://wtfpl.net
 // @license       MIT; http://opensource.org/licenses/MIT
-// @version       0.1.3
+// @version       0.2.0
 // @icon          https://raw.githubusercontent.com/dluciv/UserScripts/master/Huawei-E5372-USSD/ussd.png
 // @homepage      https://github.com/dluciv/UserScripts/tree/master/Huawei-E5372-USSD
 // @updateURL     https://raw.githubusercontent.com/dluciv/UserScripts/master/Huawei-E5372-USSD/huawei-e5372-ussd.user.js
@@ -26,5 +26,23 @@
       unsafeWindow.sendCommonByUssd(ussd, "CodeType");
     }
   };
-  GM_registerMenuCommand("USSD", doall);
+  var nop = function(){};
+  GM_registerMenuCommand("-- Any USSD --", nop);
+  GM_registerMenuCommand("USSD", doall, "u");
+
+  GM_registerMenuCommand("-- Common replies --", nop);
+  
+  var comrep = [
+    '1', '2', '3',
+    '4', '5', '6',
+    '7', '8', '9',
+    '0', '00', '000'
+  ];
+  for(var n=0; n < comrep.length; ++n){
+    (function(nn){
+      GM_registerMenuCommand("Reply " + nn, function(){
+        unsafeWindow.sendCommonByUssd(nn, "CodeType");
+      }, nn.length == 1 ? nn: null);
+    })(comrep[n]);
+  }
 }).call(this);
