932c48ae6aa36b046afca596d15adcdb775837dd
Sheroz Ashurov <sheroz_92@bk.ru>
2022-03-28 13:03:39 +0500

    Update huawei-e5372-ussd.user.js

--- Huawei-E5372-USSD/huawei-e5372-ussd.user.js
+++ Huawei-E5372-USSD/huawei-e5372-ussd.user.js
@@ -20,6 +20,7 @@
 // ==/UserScript==
 
 (function() {
+
     unsafeWindow.huawei_send_USSD = exportFunction(function(ussd_code){
         unsafeWindow.sendCommonByUssd(ussd_code, "CodeType");
     }, unsafeWindow);
@@ -28,16 +29,25 @@
         try {
             document.getElementById('huawei_ussd_keypad').remove();
         } catch (e) {}
-        document.body.innerHTML +=
-            '<div id="huawei_ussd_keypad" style="display:block; position: absolute; top:15px; right: 15px; border: 1px solid black; background-color: white; padding: 5px; width: 135px; z-index: 10000;">'+
-            '<input type="button" onclick="document.getElementById(\'huawei_ussd_keypad\').remove();" value="X" style=""/><span style="background-color: silver; height: 20px; padding: 5px;">Send or click</span><hr/>'+
-            '<input type="text" id="huawei_ussd_text" value="*100#" style="width:84px; padding:2px; border: 1px solid black;" /><input type="button" value="&gt;&gt;" onclick="huawei_send_USSD(document.getElementById(\'huawei_ussd_text\').value)" style="width:45px;" /><hr/>'+
+        var innerHTML =
+            '<div id="huawei_ussd_keypad" style="display:none; position: absolute; top:15px; right: 15px; border: 1px solid black; background-color: white; padding: 5px; width: 135px; z-index: 10000;">'+
+            '<input type="button" onclick="$(\'#huawei_ussd_keypad\').hide();" value="X" style=""/><span style="background-color: silver; height: 20px; padding: 5px;">Send or click</span><hr/>'+
+            '<input type="text" id="huawei_ussd_text" value="*114#" style="width:84px; padding:2px; border: 1px solid black;" /><input type="button" value="&gt;&gt;" onclick="huawei_send_USSD(document.getElementById(\'huawei_ussd_text\').value)" style="width:45px;" /><hr/>'+
             '<input type="button" value="1" onclick="huawei_send_USSD(\'1\')" style="width:45px;" /><input type="button" value="2" onclick="huawei_send_USSD(\'2\')" style="width:45px;" /><input type="button" value="3" onclick="huawei_send_USSD(\'3\')" style="width:45px;" /><br/>'+
             '<input type="button" value="4" onclick="huawei_send_USSD(\'4\')" style="width:45px;" /><input type="button" value="5" onclick="huawei_send_USSD(\'5\')" style="width:45px;" /><input type="button" value="6" onclick="huawei_send_USSD(\'6\')" style="width:45px;" /><br/>'+
             '<input type="button" value="7" onclick="huawei_send_USSD(\'7\')" style="width:45px;" /><input type="button" value="8" onclick="huawei_send_USSD(\'8\')" style="width:45px;" /><input type="button" value="9" onclick="huawei_send_USSD(\'9\')" style="width:45px;" /><br/>'+
             '<input type="button" value="0" onclick="huawei_send_USSD(\'0\')" style="width:45px;" /><input type="button" value="00" onclick="huawei_send_USSD(\'00\')" style="width:45px;" /><input type="button" value="000" onclick="huawei_send_USSD(\'000\')" style="width:45px;" /><br/>'+
             '</div>';
+        $('body').append(innerHTML);
     };
-   
-    GM_registerMenuCommand("USSD: show pad", addpad, "u");
+    addpad();
+    $('#union_main_menu ul').append('<li id="menu_sharing"><a onclick="$(\'#huawei_ussd_keypad\').show();" href="#" tabindex=""><span>USSD</span></a></li>');
+    setTimeout(function(){
+       if($('#username_span').text() != 'admin'){
+           $('#logout_span').click();
+           $('#username').val('admin');
+           $('#password').val('admin');
+           $('#pop_login a').click();
+       }
+    }, 1000);
 }).call(this);
